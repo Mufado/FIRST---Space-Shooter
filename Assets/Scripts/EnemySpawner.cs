f1a95eb33d7666a83d01d7c89e3d343b9c94bdea
Mufado <gustavogiraobg@gmail.com>
2023-04-17 22:18:03 -0300

    Adjust in Enemy Spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,10 +5,14 @@ public class EnemySpawner : BaseSpawner
 {
     // Needs to know if player dies to stop spawning
     private Player _player;
+    [SerializeField]
+    private float _spawnTime;
 
     protected override void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        _spawnTime = EnemySettings.Instance.SpawnTime;
+
         StartCoroutine(Spawn());
     }
 
@@ -19,7 +23,7 @@ public class EnemySpawner : BaseSpawner
             Vector3 spawnPosition = new Vector3(Random.Range(EnemySettings.Instance.LeftBound, EnemySettings.Instance.RightBound), EnemySettings.Instance.TopBound);
             GameObject newEnemy = Instantiate(_prefab, spawnPosition, Quaternion.identity);
             newEnemy.transform.parent = gameObject.transform;
-            yield return new WaitForSeconds(EnemySettings.Instance.SpawnTime);
+            yield return new WaitForSeconds(_spawnTime);
         }
     }
 }
