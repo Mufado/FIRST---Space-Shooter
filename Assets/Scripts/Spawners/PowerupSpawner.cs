1dc00e68e7a5563ea6b1962689628bed190b6a2a
Mufado <gustavogiraobg@gmail.com>
2023-04-30 15:01:22 -0300

    Add random powerup spawning

--- Assets/Scripts/Spawners/PowerupSpawner.cs
+++ Assets/Scripts/Spawners/PowerupSpawner.cs
@@ -1,10 +1,24 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerupSpawner : BaseSpawner
 {
+    [SerializeField]
+    private GameObject _speedPrefab;
+    [SerializeField]
+    private GameObject _tripleLaserPrefab;
+
+    private Dictionary<PowerupType, GameObject> _powerupPrefabs;
+
     protected void Start()
     {
+        _powerupPrefabs = new Dictionary<PowerupType, GameObject>
+        {
+            { PowerupType.Speed, _speedPrefab},
+            { PowerupType.TripleLaser, _tripleLaserPrefab}
+        };
+
         _player = GameObject.Find("Player").GetComponent<Player>();
 
         StartCoroutine(Spawn(
@@ -12,5 +26,23 @@ public class PowerupSpawner : BaseSpawner
             topPosition: PowerupSettings.Instance.DefaultTopBound,
             spawnInterval: PowerupSettings.Instance.DefaultSpawnTime
         ));
+    }
+
+    protected override IEnumerator Spawn((float leftBound, float rightBound) spawnRange, float topPosition, float spawnInterval)
+    {
+        int powerupQuantity = System.Enum.GetValues(typeof(PowerupType)).Length;
+
+        while (_player.IsAlive)
+        {
+            PowerupType powerupType = (PowerupType)Random.Range(0, powerupQuantity);
+            _powerupPrefabs.TryGetValue(powerupType, out _prefab);
+
+            Vector3 spawnPosition = new Vector3(Random.Range(spawnRange.leftBound, spawnRange.rightBound), topPosition);
+
+            GameObject spawnObject = Instantiate(_prefab, spawnPosition, Quaternion.identity);
+            spawnObject.transform.parent = gameObject.transform;
+
+            yield return new WaitForSeconds(spawnInterval);
+        }
     }
 }
